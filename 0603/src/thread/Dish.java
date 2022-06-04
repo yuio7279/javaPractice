dcab61dfae05a6ec9d5bd2c23ac9b602bc4dc46f
yuio7279 <yuio7279@gmail.com>
2022-06-04 12:21:10 +0900

    0603 / multithread

---
+++ 0603/src/thread/Dish.java
@@ -0,0 +1,13 @@
+package thread;
+
+public class Dish {
+
+	private boolean empty = true;
+	
+	public boolean isEmpty() {
+		return empty;
+	}
+	public void setEmpty(boolean empty) {
+		this.empty = empty;
+	}
+}
---
+++ 0603/src/thread/DishDemo.java
@@ -0,0 +1,12 @@
+package thread;
+
+public class DishDemo {
+
+	public static void main(String[] args) {
+		final Dish d = new Dish();
+		new Thread(new Customer(d)).start();
+		new Thread(new Cook(d)).start();
+
+	}
+
+}
